0a4073d0b964bd4779193259f4f55390f3a644da
cwnidog <cwnidog@comcast.net>
2015-09-23 14:31:31 -0700

    Adding code to create an NSDate with a specified time using a category of NSDate

--- DateMonger/main.m
+++ DateMonger/main.m
@@ -7,11 +7,18 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "NSDate+BNRDateConvenience.h"
 
-int main(int argc, const char * argv[]) {
-  @autoreleasepool {
-      // insert code here...
-      NSLog(@"Hello, World!");
-  }
+int main(int argc, const char *argv[])
+{
+  @autoreleasepool
+  {
+    NSInteger year = 2016;
+    NSInteger month = 1;
+    NSInteger day = 1;
+    
+    NSDate *NewYears = [NSDate initWithSpecifiedDate:month withDay:day withYear:year];
+    NSLog(@"News Year's Day, 2016: %@", NewYears);
+  } // @autoreleasepool
     return 0;
-}
+} // main()
--- NSDate+BNRDateConvenience.m
+++ NSDate+BNRDateConvenience.m
@@ -10,4 +10,22 @@
 
 @implementation NSDate (BNRDateConvenience)
 
++ (NSDate *)initWithSpecifiedDate:(NSInteger)month withDay:(NSInteger)day withYear:(NSInteger)year
+{
+  // create a new NSDateComponents instance and set it to midnight on the specified date
+  NSDateComponents *comps = [NSDateComponents new];
+  [comps setYear:year];
+  [comps setMonth:month];
+  [comps setDay:day];
+  [comps setHour:-8]; // allowing for Pacific time being GMT -8
+  [comps setMinute:0];
+  [comps setSecond:0];
+  
+  NSCalendar *g = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
+  NSDate *newDay = [g dateFromComponents:comps];
+  
+  return newDay;
+  
+} // initWithSpecifiedDate
+
 @end
